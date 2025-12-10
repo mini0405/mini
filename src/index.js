ebbd774b3216a2f16d805a7df5d5ed5676d22d53
mini-stuurman <stuurmanminentle04@gmail.com>
2025-12-10 02:22:47 +0200

    Add AWS CloudWatch RUM for real user monitoring and performance tracking

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AwsRum } from 'aws-rum-web';
 
+try {
+  const config = {
+    sessionSampleRate: 1 ,
+    identityPoolId: "us-east-1:f096d922-29de-464c-938e-abbee6209349" ,
+    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com" ,
+    telemetries: ["performance","errors","http"] ,
+    allowCookies: true ,
+    enableXRay: true ,
+    signing: true // If you have a public resource policy and wish to send unsigned requests please set this to false
+  };
+
+  const APPLICATION_ID = '312812f0-680b-433d-9078-635695fda9af';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'us-east-1';
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
